82a2e6cc8629d835ea8f9c6dd9390ea292462564
ivanrezic <ivan.rezic1@gmail.com>
2020-08-26 22:25:45 +0200

    Updates

---
+++ precache-manifest.424bbbd93a8814ca928eac570e83d1f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf7304bbe1ac551059f66abd7da622d",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "2e75d0a874bcdd013e3b",
+    "url": "/personal-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "744435753a6cdabd48f3",
+    "url": "/personal-website/static/js/2.9b55d0e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website/static/js/2.9b55d0e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e75d0a874bcdd013e3b",
+    "url": "/personal-website/static/js/main.d19a0196.chunk.js"
+  },
+  {
+    "revision": "d9385b05e5e57d9628fe",
+    "url": "/personal-website/static/js/runtime-main.3731dab7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
