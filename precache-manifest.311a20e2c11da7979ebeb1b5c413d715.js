7e0ce7a7d8ebab068cde412fd5ebf1f9478b6c49
ivanrezic <ivan.rezic1@gmail.com>
2020-10-10 00:11:24 +0200

    Updates

---
+++ precache-manifest.311a20e2c11da7979ebeb1b5c413d715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c254dd100a839493c6d9ff8b243a5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edf770126f081ed905ee",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "4e1f8a3260ad801e2279",
+    "url": "/static/js/2.b80d1c2f.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.b80d1c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf770126f081ed905ee",
+    "url": "/static/js/main.a4a42331.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  }
+]);
